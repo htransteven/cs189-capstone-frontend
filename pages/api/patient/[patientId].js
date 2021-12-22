39d43e604659050fc0433551fda7ac183dd2854e
Katelyn Zhang <krzhang@ucsb.edu>
2021-12-22 06:00:52 -0800

    add api route for deleting patient from patient table using patient id

--- pages/api/patient/[patientId].js
+++ pages/api/patient/[patientId].js
@@ -2,8 +2,8 @@ const configureDatabase = require('../service/configure_database');
 var ddb = configureDatabase();
 
 export default function handler(req, res) {
+  const { patientId } = req.query
   if (req.method === 'GET') {
-    const { patientId } = req.query
     var params = {
       KeyConditionExpression: 'patient_id = :patient_id',
       ExpressionAttributeValues: {
@@ -20,8 +20,24 @@ export default function handler(req, res) {
         console.log("Success", JSON.stringify(data.Items));
       }
     });
+  } else if (req.method === 'DELETE') {
+    var params = {
+      TableName: 'patient',
+      Key: {
+        'patient_id' : {N: patientId}
+      }
+    };
+  
+    ddb.deleteItem(params, function(err, data) {
+    if (err) {
+      console.log("Error", err);
+    } else {
+      res.status(200).json(data);
+      console.log("Success: deleted item from table(patient)", JSON.stringify(data));
+    }
+    });
   } else {
-    res.status(400).send({ message: 'Only GET requests allowed' })
+    res.status(400).send({ message: `${req.method} is not a valid request` })
     return
   }
 }
