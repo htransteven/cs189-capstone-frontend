264ee94b640a1c7f5b639c3c19c281123ae22484
Katelyn Zhang <krzhang@ucsb.edu>
2022-01-10 03:22:38 -0800

    add api route for retreiving all appointments of a patient using patient id

--- pages/api/appointments/index.js
+++ pages/api/appointments/index.js
@@ -1,4 +1,6 @@
 const putDatabaseInfo = require("../service/database")
+const configureDatabase = require('../service/configure_database');
+var ddb = configureDatabase();
 
 export default function handler(req, res) {
   if (req.method === 'POST' || req.method === 'PUT') {
@@ -6,6 +8,31 @@ export default function handler(req, res) {
     const body = JSON.parse(req.body)
     putDatabaseInfo(table_name, body);
     res.status(200).json(body);
+  } else if (req.method === 'GET') {
+    const query = (req.query)
+    if (query.patient_id) {
+      const pat_id = query.patient_id
+      var params={
+        TableName: 'appointment',
+        IndexName: 'patient_id-index',
+        KeyConditionExpression: 'patient_id = :patient_id',
+        ExpressionAttributeValues: {
+          ':patient_id': {'N': pat_id}
+        }
+      }
+
+      ddb.query(params, function(err, data) {
+        if (err) {
+          console.log("Error", err);
+        } else {
+          res.status(200).json(data.Items)
+          console.log("Success", data.Items);
+        }
+      });
+
+    } else {
+      res.status(400).send({ message: `Query params do not contain patient_id` })
+    }
   } else {
     res.status(400).send({ message: `${req.method} is not a valid request` })
     return
