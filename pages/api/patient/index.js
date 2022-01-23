2547d8b607613d6d19fb88511aeaca10326723ef
Evan Yip <etyip@ucsb.edu>
2022-01-23 13:26:36 -0800

    update patient insert endpoint

--- pages/api/patient/index.js
+++ pages/api/patient/index.js
@@ -3,9 +3,8 @@ const putDatabaseInfo = require("../../../service/database")
 export default function handler(req, res) {
   if (req.method === 'POST' || req.method === 'PUT') {
     const table_name = "patients"
-    const body = JSON.parse(req.body)
-    putDatabaseInfo(table_name, body);
-    res.status(200).json(body);
+    putDatabaseInfo(table_name, req.body);
+    res.status(200).send(req.body);
   } else {
     res.status(400).send({ message: `${req.method} is not a valid request` })
     return
