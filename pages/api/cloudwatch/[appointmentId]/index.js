2bebaf00db87c516548ed37bcf2b0e9f70c93e71
Katelyn Zhang <krzhang@ucsb.edu>
2022-01-23 17:53:18 -0800

    reformat

--- pages/api/cloudwatch/[appointmentId]/index.js
+++ pages/api/cloudwatch/[appointmentId]/index.js
@@ -9,9 +9,9 @@ export default async function handler(req, res) {
       const result = cw.startQuery({
               logGroupName: "brad_convos",
               queryString: `fields @timestamp, @message
-        | sort @timestamp desc
-        | filter slots.AppointmentNumber = ${appointment_id}
-        | limit 10`,
+                | sort @timestamp desc
+                | filter slots.AppointmentNumber = ${appointmentId}
+                | limit 10`,
               startTime: new Date("2021-09-12").getTime(),
               endTime: new Date().getTime(),
               limit: 10
@@ -24,32 +24,35 @@ export default async function handler(req, res) {
                     }
                     if (response.status !== "Running") {
                       clearInterval(id)
-            //These two aren't really necessary but it gives everything in the query/log if it's needed
-            res.send(response)
+
+                      //These two aren't really necessary but it gives everything in the query/log if it's needed
+                      res.send(response)
                       console.log(response.results)
             
-            //Give to Frontend:This console log gives the date/timestamp of the log (ex.2022-01-11 20:38:44.087)
-            console.log((response.results[0][0]).value)
+                      //Give to Frontend:This console log gives the date/timestamp of the log (ex.2022-01-11 20:38:44.087)
+                      console.log((response.results[0][0]).value)
+                      
+                      const text =String((response.results[0][1]).value)
+                      const obj=JSON.parse((text))
             
-            const text =String((response.results[0][1]).value)
-            const obj=JSON.parse((text))
-  
-            //Give to Frontend:This console log gives the user-input/message to the bot
-            console.log(obj.inputTranscript)
-  
-            //Give to Frontend:This console log gives the bot-response
-            console.log(obj.botResponse)
+                      //Give to Frontend:This console log gives the user-input/message to the bot
+                      console.log(obj.inputTranscript)
             
+                      //Give to Frontend:This console log gives the bot-response
+                      console.log(obj.botResponse)
+                      
                       resolve(response.results)
-            //console.log( (response.results[0][1]).value )
-            //console.log(text)
+                      //console.log( (response.results[0][1]).value )
+                      //console.log(text)
                     }
-                  })
+                  }
+                )
                }, 10000)
-              
             }
           )
-      })
-  
+      }
+    )
+  } else {
+    res.status(400).send({ message: `${req.method} is not a valid request` });
   }
 }
