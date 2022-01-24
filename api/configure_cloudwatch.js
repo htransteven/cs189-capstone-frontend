406014fd2cdf0bbd944c27955307ad867860a658
Katelyn Zhang <krzhang@ucsb.edu>
2022-01-23 19:41:14 -0800

    add configs to cloudwatch setup to resolve region error

--- api/configure_cloudwatch.js
+++ api/configure_cloudwatch.js
@@ -1,5 +1,12 @@
 // Load the AWS SDK for Node.js
 var AWS = require('aws-sdk');
+// Set configs
+AWS.config.update({
+  "region": "us-west-2",
+  "accessKeyId": process.env.NEXT_AWS_DYNAMO_ACCESS_KEY_ID,
+  "secretAccessKey": process.env.NEXT_AWS_DYNAMO_SECRET_ACCESS_KEY
+});
+
 var cw = new AWS.CloudWatchLogs();
 
 module.exports = () => {
