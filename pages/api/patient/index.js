4beaf6925383e8873b1f1ba368038416a2d5ba85
Katelyn Zhang <krzhang@ucsb.edu>
2022-01-31 02:30:07 -0800

    all items in all tables are automatically marshalled for putitem

--- pages/api/patient/index.js
+++ pages/api/patient/index.js
@@ -3,7 +3,8 @@ const putDatabaseInfo = require("../../../api-utils/database");
 export default function handler(req, res) {
   if (req.method === 'POST' || req.method === 'PUT') {
     const table_name = "patients"
-    putDatabaseInfo(table_name, req.body);
+    const body = JSON.parse(req.body);
+    putDatabaseInfo(table_name, body);
     res.status(200).send(req.body);
   } else {
     res.status(400).send({ message: `${req.method} is not a valid request` });
