11937657eaabfe748cb67452c1fa8b66feac92d7
Katelyn Zhang <krzhang@ucsb.edu>
2022-02-01 14:12:13 -0800

    remove general_consults table usage

--- api-utils/database.js
+++ api-utils/database.js
@@ -3,13 +3,13 @@ var ddb = configureDatabase();
 var AWS = require('aws-sdk');
 
 module.exports = (table_name, params) => {
-  if (!params.appointment_id && (table_name == "appointments" || table_name == "general_consults")) {
+  if (!params.appointment_id && (table_name == "appointments")) {
     console.log("Error: missing required appointment_id key");
   }
   if (!params.doctor_id && (table_name == "doctors")) {
     console.log("Error: missing required doctor_id key");
   }
-  if (!params.patient_id && (table_name == "patients" || table_name == "appointments" || table_name == "general_consults")) {
+  if (!params.patient_id && (table_name == "patients" || table_name == "appointments")) {
     console.log("Error: missing required patient_id key");
   }
 
