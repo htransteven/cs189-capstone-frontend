c3e6e30a00d3fdd5ca3444503fdc3b97a3b072ff
Evan Yip <etyip@ucsb.edu>
2022-02-02 11:56:06 -0800

    Have redirect to dashboard

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,26 +1,36 @@
-import { useUser } from "../contexts/UserContext";
+import { useRouter } from 'next/router';
+import { useRole, useUser } from '../contexts/UserContext';
 
 const Navbar = () => {
   const user = useUser();
+  const router = useRouter();
+
   return (
-    <div>
-      <header className='px-5 border-b'>
-        <div className='grid grid-cols-6 gap-4 py-4'>
-          <h1 className='font-mono text-3xl font-bold text-gray-900'>SCRIBE</h1>
+    <div className="min-h-full">
+      <header className="px-5 border-b">
+        <div className="grid grid-cols-6 gap-4 py-4">
+          <h1
+            onClick={() => {
+              router.push('/');
+            }}
+            className="font-mono text-3xl font-bold text-gray-900 hover:cursor-pointer"
+          >
+            SCRIBE
+          </h1>
         </div>
       </header>
-      <header className='px-5 border-b'>
-        <div className='grid grid-cols-6 gap-4 py-4'>
-          <div className='col-span-4'>
-            <p className='text-md'>Hello, {user.name}</p>
+      <header className="px-5 border-b">
+        <div className="grid grid-cols-6 gap-4 py-4">
+          <div className="col-span-4">
+            <p className="text-md">Hello, {user.name}</p>
           </div>
-          <div className='col-span-2 justify-self-end'>
+          <div className="col-span-2 justify-self-end">
             <button
-              type='button'
-              className='w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm'
+              type="button"
+              className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
             >
               {/* eslint-disable-next-line */}
-              <a href='/api/auth/logout'>Logout</a>
+              <a href="/api/auth/logout">Logout</a>
             </button>
           </div>
         </div>
