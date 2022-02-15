c35db6c39ddb1adf55a3bc453ca7aff7e0791bd0
Steven Huynh-Tran <htransteven@gmail.com>
2022-02-15 11:23:50 -0800

    fix time for patient view appointments

--- pages/patient/index.tsx
+++ pages/patient/index.tsx
@@ -1,13 +1,13 @@
-import { withPageAuthRequired } from '@auth0/nextjs-auth0';
-import React, { useEffect, useState } from 'react';
-import { Appointment, Doctor, Patient } from '../../api-utils/models';
-import { useApi } from '../../contexts/APIClientContext';
-import { useRole, useUser } from '../../contexts/UserContext';
-import { ChatBot } from '../../components';
-import Navbar from '../../components/Navbar';
-import { format, fromUnixTime } from 'date-fns';
-import Tag from '../../components/Tag';
-import Link from 'next/link';
+import { withPageAuthRequired } from "@auth0/nextjs-auth0";
+import React, { useEffect, useState } from "react";
+import { Appointment, Doctor, Patient } from "../../api-utils/models";
+import { useApi } from "../../contexts/APIClientContext";
+import { useRole, useUser } from "../../contexts/UserContext";
+import { ChatBot } from "../../components";
+import Navbar from "../../components/Navbar";
+import { format, fromUnixTime } from "date-fns";
+import Tag from "../../components/Tag";
+import Link from "next/link";
 
 export const AppointmentCard: React.FC<Appointment> = ({
   appointment_id,
@@ -51,89 +51,89 @@ export const AppointmentCard: React.FC<Appointment> = ({
   console.log(patient);
 
   return (
-    <div className="w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4">
+    <div className='w-full sm:w-1/2 md:w-1/2 xl:w-1/4 p-4'>
       <Link href={`/${role}/appointments/${appointment_id}`}>
         <a
-          href=""
-          className="block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden h-auto"
+          href=''
+          className='block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden h-auto'
         >
-          <header className="font-bold bg-purple-800 h-16 justify-center">
-            <div className="flex justify-center pt-1">
-              <h2 className="font-bold text-white">
+          <header className='font-bold bg-purple-800 h-16 justify-center'>
+            <div className='flex justify-center pt-1'>
+              <h2 className='font-bold text-white'>
                 Appointment #{appointment_id}
               </h2>
             </div>
-            <div className="flex justify-center pt-1">
-              <h2 className="font-bold text-white">
+            <div className='flex justify-center pt-1'>
+              <h2 className='font-bold text-white'>
                 {format(
-                  fromUnixTime(appointment_time),
-                  'EEEE, MM/dd/yyyy @ hh:mm a'
+                  fromUnixTime(appointment_time / 1000),
+                  "EEEE, MM/dd/yyyy @ hh:mm a"
                 )}
               </h2>
             </div>
           </header>
-          <div className="p-4">
-            <div className="flex flex-col">
-              <p className="text-purple-700 text-bold">Location</p>
-              <p className="text-sm">
-                {loading && 'Loading doctor data...'}
-                {!loading && !doctor && 'Failed when finding doctor...'}
+          <div className='p-4'>
+            <div className='flex flex-col'>
+              <p className='text-purple-700 text-bold'>Location</p>
+              <p className='text-sm'>
+                {loading && "Loading doctor data..."}
+                {!loading && !doctor && "Failed when finding doctor..."}
                 {doctor &&
                   `${doctor.location.address_line1} ${doctor.location.address_line2}, ${doctor.location.city}, ${doctor.location.state} ${doctor.location.postal_code}`}
               </p>
             </div>
-            <div className="mt-3 flex flex-col">
-              {loading && 'Loading patient data...'}
-              {!loading && !patient && 'Failed when finding patient data...'}
+            <div className='mt-3 flex flex-col'>
+              {loading && "Loading patient data..."}
+              {!loading && !patient && "Failed when finding patient data..."}
               {patient && (
                 <>
-                  <p className="text-purple-700 text-bold">
+                  <p className='text-purple-700 text-bold'>
                     Patient Information
                   </p>
-                  <div className="inline-flex">
-                    <p className="text-sm text-gray-600 pr-1">Name: </p>
-                    <p className="text-sm">
+                  <div className='inline-flex'>
+                    <p className='text-sm text-gray-600 pr-1'>Name: </p>
+                    <p className='text-sm'>
                       {patient.first_name} {patient.last_name}
                     </p>
                   </div>
-                  <div className="inline-flex">
-                    <p className="text-sm text-gray-600 pr-1">Birthday: </p>
-                    <p className="text-sm">
-                      {format(fromUnixTime(patient.birthday), 'MM/dd/yyyy')}
+                  <div className='inline-flex'>
+                    <p className='text-sm text-gray-600 pr-1'>Birthday: </p>
+                    <p className='text-sm'>
+                      {format(fromUnixTime(patient.birthday), "MM/dd/yyyy")}
                     </p>
                   </div>
                 </>
               )}
             </div>
 
-            <p className="text-purple-700 text-bold mt-3">Tags</p>
-            {loading && 'Loading patient data...'}
-            {!loading && !patient && 'Failed when finding patient data...'}
+            <p className='text-purple-700 text-bold mt-3'>Tags</p>
+            {loading && "Loading patient data..."}
+            {!loading && !patient && "Failed when finding patient data..."}
             {patient && (
               <div
-                className="items-center justify-items-center w-full whitespace-nowrap"
+                className='items-center justify-items-center w-full whitespace-nowrap'
                 style={{
-                  display: 'grid',
-                  gridAutoFlow: 'column',
-                  justifyContent: 'left',
-                  gridGap: '5px',
-                  padding: '5px 0px',
+                  display: "grid",
+                  gridAutoFlow: "column",
+                  justifyContent: "left",
+                  gridGap: "5px",
+                  padding: "5px 0px",
                 }}
               >
                 {patient.preexisting_conditions.map((condition, index) => (
                   <Tag
                     key={`pe-${index}-${condition.id}`}
                     text={condition.name}
-                    background="bg-gray-200"
-                    textColor="text-gray-700"
+                    background='bg-gray-200'
+                    textColor='text-gray-700'
                   />
                 ))}
               </div>
             )}
           </div>
 
-          <div className="p-4 border-t border-b text-xs text-gray-700">
-            <span className="flex items-center">See Report</span>
+          <div className='p-4 border-t border-b text-xs text-gray-700'>
+            <span className='flex items-center'>See Report</span>
           </div>
         </a>
       </Link>
@@ -147,7 +147,7 @@ const PatientHome = () => {
   const apiClient = useApi();
   const [loading, setLoading] = useState(false);
   const [appointments, setAppointments] = useState<Appointment[] | null>(null);
-  const [filter, setFilter] = useState<string>('Upcoming');
+  const [filter, setFilter] = useState<string>("Upcoming");
 
   useEffect(() => {
     const getAppointments = async () => {
@@ -169,7 +169,7 @@ const PatientHome = () => {
     const currentDate = Date.now();
     const filteredAppointments = appointments
       .filter((appointment) => {
-        if (filter === 'Upcoming') {
+        if (filter === "Upcoming") {
           return appointment.appointment_time > currentDate;
         } else {
           return appointment.appointment_time < currentDate;
@@ -183,10 +183,10 @@ const PatientHome = () => {
   return (
     <>
       <Navbar />
-      <div className="container flex w-full my-4 mx-auto items-center justify-end">
-        <label className="mx-2">Filter Appointments</label>
+      <div className='container flex w-full my-4 mx-auto items-center justify-end'>
+        <label className='mx-2'>Filter Appointments</label>
         <select
-          className="
+          className='
                     mr-5
                     block
                     w-fit
@@ -194,7 +194,7 @@ const PatientHome = () => {
                     border-gray-300
                     shadow-sm
                     focus:border-purple-300 focus:ring focus:ring-purple-200 focus:ring-opacity-50
-                  "
+                  '
           onChange={(e) => setFilter(e.target.value)}
         >
           <option>Upcoming</option>
@@ -202,10 +202,10 @@ const PatientHome = () => {
         </select>
       </div>
 
-      <div className="container mx-auto">
-        <div className="flex flex-wrap -mx-4">
-          {loading && 'Loading appointments...'}
-          {!loading && !appointments && 'No appointment data found'}
+      <div className='container mx-auto'>
+        <div className='flex flex-wrap -mx-4'>
+          {loading && "Loading appointments..."}
+          {!loading && !appointments && "No appointment data found"}
           {appointments &&
             filterAppointments(appointments).map((appointment) => (
               <AppointmentCard
